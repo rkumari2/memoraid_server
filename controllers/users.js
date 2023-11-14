1727f92462e613792c18b6066b24175678155c09
“rkumari2” <rubinakumari@mail.com>
2023-11-14 13:55:29 +0000

    updated output of token creation

--- controllers/users.js
+++ controllers/users.js
@@ -48,7 +48,7 @@ async function login (req, res) {
             throw new Error ('Wrong Credentials')
         } else {
             const token = await Token.create(user.id)
-            res.status(200).json({authenticated: true, token: token.token})
+            res.status(200).json({authenticated: true, token: token.token, token_id: token.id})
         }
     } catch (err) {
         res.status(401).json({error: err.message})
