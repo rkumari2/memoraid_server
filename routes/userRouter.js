cb95395ac785259b3c1261f828178f2c950910be
“rkumari2” <rubinakumari@mail.com>
2023-11-14 16:06:27 +0000

    fixed bug

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,6 @@ userRouter.get('/', userController.index)
 userRouter.get('/tokens', userController.tokenIndex)
 userRouter.post('/register', userController.register)
 userRouter.post('/login', userController.login)
-userRouter.delete('/tokens/:user_id', userController.destroy)
+userRouter.delete('/tokens/:id', userController.destroy)
 
 module.exports = userRouter;
